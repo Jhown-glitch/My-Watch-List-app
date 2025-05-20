f0aaa6cdafa53b78def486932d49481789cb456e
Davi Miguel <74515558+Jhown-glitch@users.noreply.github.com>
2025-05-20 14:54:46 -0300

    Correção tela detalhes

--- app/src/main/java/com/matstudios/mywatchlist/DetailContentActivity.kt
+++ app/src/main/java/com/matstudios/mywatchlist/DetailContentActivity.kt
@@ -1,28 +1,20 @@
 package com.matstudios.mywatchlist
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.matstudios.mywatchlist.databinding.DetailContentBinding
-import jp.wasabeef.glide.transformations.BlurTransformation
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class DetailContentActivity : AppCompatActivity() {
-
-    private lateinit var binding: DetailContentBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.detail_content)
-
-        binding = DetailContentBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val capaUrl = intent.getStringExtra("capaUrl")
-
-        Glide.with(this)
-            .load(capaUrl)
-            .transform(BlurTransformation(25, 3))
-            .into(binding.fundoBlur)
-
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_content)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
